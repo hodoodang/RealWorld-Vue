f5eacf95b2aaa7fc93bf6dde88b3f231abcee25c
thsagong <thsagong@wisenut.co.kr>
2020-09-28 13:42:54 +0900

    init commit

---
+++ src/store/modules/users.js
@@ -0,0 +1,37 @@
+import { api, setToken } from "../api";
+export default {
+  namespaced: true,
+  state: {
+    user: null,
+    profile: null
+  },
+  getters: {},
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    }
+  },
+  actions: {
+    getUser: async function({ commit }) {
+      const user = await api.get("/user");
+      commit("setUser", user);
+    },
+    loginUser: async function({ commit }, { email, password }) {
+      try {
+        const response = await api.post("/users/login", {
+          user: {
+            email,
+            password
+          }
+        });
+        if (response.data.user) {
+          setToken(response.data.user.token);
+          commit("setUser", response.data.user);
+        }
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    }
+  }
+};
